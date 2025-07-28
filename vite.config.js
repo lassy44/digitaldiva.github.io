1de1ae06b2a63686fbaa89618f89a26eee1c983f
lassy <176570525+lassy44@users.noreply.github.com>
2025-07-28 00:11:10 -0700

    changed icons

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build:{
+    rollupOptions:{
+      external:[
+        "@fortawesome/fontawesome-svg-core",
+        "@fortawesome/free-brands-svg-icons",
+        "@fortawesome/free-regular-svg-icons",
+        "@fortawesome/free-solid-svg-icons",
+        "@fortawesome/react-fontawesome" ,
+      
+      ]
+    }
+  }
 })
