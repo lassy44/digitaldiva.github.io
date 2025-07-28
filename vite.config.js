7776f1ed7fe17bf810fc4d0bc3410314fe04b06a
lassy <176570525+lassy44@users.noreply.github.com>
2025-07-28 01:13:24 -0700

    ignored envz

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
   build:{
     rollupOptions:{
       external:[
+        "@fortawesome/react-fontawesome",
         "@fortawesome/fontawesome-svg-core",
         "@fortawesome/free-brands-svg-icons",
         "@fortawesome/free-regular-svg-icons",
